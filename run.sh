def3948809e76fcd7be8a78115efa645926d385a
cvalliet <cvalliet@yahoo.fr>
2017-02-21 14:11:33 +0100

    fix errors

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 P1=$1
 P2=$2
-P3=$3
 
 MONGODB_NAME=${P1:-test}
-MONGODB_PATH=${P2:-/opt/mongodb/$MONGODB_NAME}
-MONGODB_PORT=${P3:-27017}
+MONGODB_PORT=${P2:-27017}
+MONGODB_PATH=/opt/mongodb/$MONGODB_NAME
+
 DOCKER_TAG=mongodb_$MONGODB_NAME
 
 mkdir -p $MONGODB_PATH/data/db
 
-echo docker build -t $DOCKER_TAG .
-echo docker run -v $MONGODB_PATH/data/db:/data/db -p $MONGODB_PORT:27017 --rm $DOCKER_TAG
+docker build -t $DOCKER_TAG . && \
+docker run -v $MONGODB_PATH/data/db:/data/db -p $MONGODB_PORT:27017 --rm $DOCKER_TAG
